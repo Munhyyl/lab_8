9af58bc5065b90499019051c73a9c1b7eab58886
munhyyl <munhyyl@live.com>
2023-12-11 17:30:07 +0800

    hevleltiig door in gargadag bolson

--- hw1.php
+++ hw1.php
@@ -7,17 +7,11 @@
 <?php
 $fahrenheit = isset($_GET['fahrenheit']) ? $_GET['fahrenheit'] : null;
 
-if (!is_null($fahrenheit)) {
-    $celsius = ($fahrenheit - 32) * 5 / 9;
-    printf("%.2fF is %.2fC<br>", $fahrenheit, $celsius);
-}
+
 
 $celsius1 = isset($_GET['celsius']) ? $_GET['celsius'] : null;
 
-if (!is_null($celsius1)) {
-    $fahrenheit1 = $celsius1 * 9 / 5 + 32;
-    printf("%.2fC is %.2fF", $celsius1, $fahrenheit1);
-}
+
 ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
@@ -31,6 +25,17 @@ if (!is_null($celsius1)) {
     <input type="text" name="celsius" value="<?php echo $celsius1; ?>" /><br />
     <input type="submit" value="Convert to Fahrenheit!" />
 </form>
+<?php
+if (!is_null($fahrenheit)) {
+    $celsius = ($fahrenheit - 32) * 5 / 9;
+    printf("%.2fF is %.2fC<br>", $fahrenheit, $celsius);
+}
+
+if (!is_null($celsius1)) {
+    $fahrenheit1 = $celsius1 * 9 / 5 + 32;
+    printf("%.2fC is %.2fF", $celsius1, $fahrenheit1);
+}
+?>
 
 </body>
 </html>
